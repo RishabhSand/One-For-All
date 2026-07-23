449e1387c6add714915a8163c11b0f5e20032c8f
Rishabh Sand <rishabhsand2007@gmail.com>
2026-07-23 11:22:43 +0530

    Delete roommate finder/src directory

--- roommate finder/src/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    // Allows this temporary Vite site to be reached through an ngrok tunnel.
-    allowedHosts: ['backstage-aspire-unfilled.ngrok-free.dev'],
-  },
-});
